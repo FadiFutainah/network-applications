187647e316d425b8cb54acea345832451bd6172e
Fadi Ft Ftena <fadiftenaft@gmail.com>
2023-12-16 22:43:52 +0300

    upload multi files

--- files/views.py
+++ files/views.py
@@ -1,18 +1,22 @@
 import os
 
 from django.shortcuts import render
+
+from files.models import File
 from frive import settings
 
 
 def upload_file(request):
     if request.method == 'POST':
-        uploaded_file = request.FILES['file']
+        uploaded_files = request.FILES.getlist('file')
 
-        save_path = os.path.join(settings.MEDIA_ROOT, uploaded_file.name)
-
-        with open(save_path, 'wb') as destination:
-            for chunk in uploaded_file.chunks():
-                destination.write(chunk)
+        for uploaded_file in uploaded_files:
+            save_path = os.path.join(settings.MEDIA_ROOT, uploaded_file.name)
+            with open(save_path, 'wb') as destination:
+                for chunk in uploaded_file.chunks():
+                    destination.write(chunk)
+            file = File(name=uploaded_file.name)
+            file.save()
 
         return render(request, 'upload_success.html')
 
