b7d631611c8c14dd605bae1a5cac33877d68c8db
Fadi Ft Ftena <fadiftenaft@gmail.com>
2023-12-16 22:18:01 +0300

    edit files groups logic

--- files/models.py
+++ files/models.py
@@ -2,11 +2,12 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class FilesGroup(models.Model):
+    name = models.CharField(max_length=50, default='group')
+    users = models.ManyToManyField(User)
+
+
 class File(models.Model):
     name = models.CharField(max_length=50)
     editor_id = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
-
-
-class FilesGroup(models.Model):
-    files = models.ManyToManyField(File)
-    users = models.ManyToManyField(User)
+    groups = models.ManyToManyField(FilesGroup)
