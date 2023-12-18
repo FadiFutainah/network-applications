d817c88fc1200a63308aca21286be722ec839d96
Fadi Ft Ftena <fadiftenaft@gmail.com>
2023-12-18 14:04:54 +0300

    edit validation on file serializer

--- files/serializers.py
+++ files/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from files.models import File, Log
 
@@ -7,6 +8,11 @@ class FileSerializer(serializers.ModelSerializer):
     class Meta:
         model = File
         fields = ['id', 'name', 'file']
+
+    def validate(self, attrs):
+        if attrs.get('file') is None:
+            raise ValidationError()
+        return attrs
 
 
 class LogSerializer(serializers.ModelSerializer):
