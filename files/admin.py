1b3c7b78e7c1dbd7df48c5b754e25f6550ed5a6a
Fadi Ft Ftena <fadiftenaft@gmail.com>
2023-12-18 14:31:45 +0300

    remove add permession on log

--- files/admin.py
+++ files/admin.py
@@ -2,4 +2,18 @@ from django.contrib import admin
 
 from files.models import File, FilesGroup, Log
 
-admin.site.register([File, FilesGroup, Log])
+
+@admin.register(Log)
+class LogAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request, obj=None):
+        return False
+
+
+@admin.register(File)
+class LogAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(FilesGroup)
+class LogAdmin(admin.ModelAdmin):
+    pass
